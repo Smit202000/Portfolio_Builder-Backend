2c096077646bf794973500240b5c51b7fb0d55e8
Nikhil Jain <nikhil.j@simformsolutions.com>
2022-04-02 19:19:31 +0530

    Cloudinary file upload done

--- app.js
+++ app.js
@@ -4,18 +4,20 @@ const mongoose = require('mongoose');
 const bodyparser = require('body-parser');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const multer = require('multer');
-
 const fetchResumeDataRouter = require('./routes/fetch-resume-data.routes');
-const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'resume');
-  },
-  filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
-  },
-});
-
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+const Datauri = require('datauri');
+// file upload in local
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'resume');
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, new Date().toISOString() + '-' + file.originalname);
+//   },
+// });
+const storage = multer.memoryStorage();
 const fileFilter = (req, file, cb) => {
   if (
     file.mimetype === 'application/pdf' ||
@@ -28,14 +30,24 @@ const fileFilter = (req, file, cb) => {
 };
 
 dotenv.config();
+console.log(process.env.CLOUDINARY_API_KEY);
+
 const app = express();
 app.use(bodyparser.json());
-app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
-);
-app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// app.use(multer({ storage: storage, fileFilter: fileFilter }).single('image'));
+app.use(multer({ storage: storage }).single('image'));
+app.use(multer({ storage: storage }).single('image'));
+app.use('/images', express.static(path.join(__dirname, 'image')));
 app.use('/resume', express.static(path.join(__dirname, 'resume')));
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(cors());
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 app.use(fetchResumeDataRouter);
 
 const connectDb = async () => {
@@ -45,7 +57,7 @@ const connectDb = async () => {
       console.log('Server started on port: ', process.env.PORT);
     });
   } catch (error) {
-    console.log(error);
+    console.log(error, 'error');
   }
 };
 connectDb();
--- controllers/fetch-resume-data.controller.js
+++ controllers/fetch-resume-data.controller.js
@@ -1,15 +1,45 @@
 const { userModel } = require('../models/user');
 const fs = require('fs');
 const pdf = require('pdf-parse');
+const DatauriParser = require('datauri/parser');
+const parser = new DatauriParser();
+const path = require('path');
+const { uploader } = require('cloudinary');
+const dataUri = async (req) =>
+  await parser.format(
+    path.extname(req.file.originalname).toString(),
+    req.file.buffer
+  );
+exports.uploadResume = async (req, res, next) => {
+  const resumeUrl = req.file;
+  console.log(resumeUrl, 'resmue url');
+  if (req.file) {
+    try {
+      const file = await dataUri(req);
+      // console.log(file.content);
+      const result = await uploader.upload(file.content);
 
-exports.uploadResume = (req, res, next) => {
-  const resumeUrl = req.file.path;
+      const image = result.url;
+      return res.status(200).json({
+        messge: 'Your image has been uploded successfully to cloudinary',
+        data: {
+          image,
+        },
+      });
+    } catch (err) {
+      res.status(400).json({
+        messge: 'someting went wrong while processing your request',
+        data: {
+          err,
+        },
+      });
+    }
+  }
 };
 
 exports.fetchData = (req, res, next) => {
-  console.log(__dirname);
+  //   console.log(__dirname);
   //   let dataBuffer = fs.readFileSync();
-
   //   pdf(dataBuffer)
   //     .then((d) => {
   //       console.log(d);
@@ -18,3 +48,4 @@ exports.fetchData = (req, res, next) => {
   //       console.log(e);
   //     });
 };
+//IMPORT THE MODEL WE CREATED EARLIER
