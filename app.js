cfb7f5ce5d7bd4b04cc5e9a167244edf34685122
parag-10 <parag.b@simformsolutions.com>
2022-04-02 14:47:56 +0530

    Login Logout using JWT

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ const mongoose = require('mongoose');
 const bodyparser = require('body-parser');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const userRouter = require("./routes/user.routes");
+const { errorHandler } = require('./middlewares/errorHandler');
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(bodyparser.json());
 app.use('/images', express.static(path.join(__dirname, 'images')));
-
+app.use("/user", userRouter)
+app.use(errorHandler)
+// const User = require("./models/user")
+// ====
+// const u = User({first_name:"a", last_name:"b", email:"a@a.a", password:"1", user_name:"a"})
+// u.save().then(_=>console.log(_)).catch(e=>console.log(e.message))
+// =======
 const connectDb = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL);
---
+++ routes/user.routes.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const { loginController, logoutController, refreshAccessToken } = require("../controllers/user.js")
+
+router = express.Router()
+
+router.post("/login", loginController)
+
+router.post("/logout", logoutController)
+
+router.post("/refresh-access", refreshAccessToken)
+module.exports = router
